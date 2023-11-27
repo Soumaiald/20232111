9f163d1c10ae56aca7c7906aec87007764268fe6
Soumaia <94118305+Soumaiald@users.noreply.github.com>
2023-11-27 19:06:48 +0100

    Create exercice.js

---
+++ exercice.js
@@ -0,0 +1,30 @@
+// Variables declaration example
+var newVariable = "string value"
+let newBooleen = true
+const newCst = 7
+
+/**
+ * this an example of "if else" statement
+ */
+
+if (newBooleen == true) {
+    console.log("the if condition is True")
+}
+else {
+    console.log("the condition is False")
+}
+
+var a=3
+var b=5
+const fruits = ['apple', 'banana', 'cherry']
+
+
+if (a>b && fruits.length==3){
+    console.log(a + " is greater than " + b + "and fruits length equal to 3")
+}
+else if (a-b<0) {
+    console.log(a + " is less than " + b)
+}
+else {
+    console.log(a + " is equal " + b)
+}
